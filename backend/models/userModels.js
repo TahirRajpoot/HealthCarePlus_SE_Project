d293b17848d564a7b902f1e5bd359c61f6e5b21c
Muhammad Usman Asghar <42khan0@gmail.com>
2023-11-25 19:50:39 +0500

    update usermodels

--- backend/models/userModels.js
+++ backend/models/userModels.js
@@ -1,24 +1,57 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema(
-    {
-        name: String,
-        username: {
-            type: String,
-            unique: true,
-        },
-        email: {
-            type: String,
-            unique: true,
-        },
-        password: {
-            type: String,
-            unique: true,
-        },
-        role: String,
+  {
+    name: {
+      type: String,
+      required: true,
     },
-    { timestamps: true }
+    email: {
+      type: String,
+      unique: true,
+    },
+    password: {
+      type: String,
+      unique: true,
+    },
+    dob: {
+      type: String,
+    },
+    age: {
+      type: Number,
+    },
+    contacts: {
+      type: Number,
+    },
+    appointmentsmade: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Appointments",
+    },
+
+    isDisabled: {
+      type: Boolean,
+    },
+    medicines: [
+      {
+        desc: String,
+        disease: String,
+        date1: [Date],
+      },
+    ],
+    report: [
+      {
+        type: String,
+        img: String,
+        desc: String,
+        title: String,
+        date: Date.now,
+      },
+    ],
+    reports: {
+      type: String,
+    },
+  },
+  { timestamps: true }
 );
 
-
-module.exports = mongoose.model("Users Credentials", UserSchema);
+module.exports = mongoose.model("Users", UserSchema);
