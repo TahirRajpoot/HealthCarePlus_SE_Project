69786d07aecfe43e2ea4cc4ff4c8a15b8599ddfb
Muhammad Usman Asghar <42khan0@gmail.com>
2023-11-26 09:21:33 +0500

    update userroutes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const usercontroller = require("../controllers/userController");
 const logincontroller = require("../controllers/loginController");
 
-route.post("/register", usercontroller.userRegister);
-route.post("/login", logincontroller.LoginUser);
+router.post("/register", usercontroller.userRegister);
+router.post("/login", logincontroller.LoginUser);
 
 module.exports = router;
