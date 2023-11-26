6b10365ff19f43407b26d3b0a8f5adbbdb35020e
Muhammad Usman Asghar <42khan0@gmail.com>
2023-11-26 09:30:44 +0500

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,30 @@
 const express = require("express");
 const cors = require("cors");
 const bodyparser = require("body-parser");
+const app = express();
 
 const port = 8080;
+const dbconnection = require("./utilis/db");
 
-require("./utilis/db");
-const app = express();
-const userroutes = require("./routes/userRoutes")
-app.use(cors());
+const userroutes = require("./routes/userRoutes");
+const appointmentroutes = require("./routes/apppointmentRoutes");
+const doctorroutes = require("./routes/doctorRoutes");
+const hospitalroutes = require("./routes/hospitalRoutes");
+const medicroutes = require("./routes/medicRoutes");
+
 app.use(bodyparser.json());
+app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
 
+app.use("/api/user", userroutes);
+app.use("/api/userAppointment", appointmentroutes);
+app.use("/api/doctor", doctorroutes);
+app.use("/api/hospitals", hospitalroutes);
+api.use("/api/medic", medicroutes);
 
-app.use("/api", userroutes);
+dbconnection();
 
-
-
-
-
-app.listen(port, ()=>{
-    console.log(`Port  is listening at local host ${port}`);
+app.listen(port, () => {
+  console.log(`Port  is listening at local host ${port}`);
 });
-
